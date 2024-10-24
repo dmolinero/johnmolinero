1b2ee0bf97d747cdace9dd06379309967f875a62
Dave <molinero.david@gmail.com>
2024-10-23 21:59:23 -0400

    Update art

--- js/art-view.js
+++ js/art-view.js
@@ -1,63 +1,82 @@
 class ArtView {
+  illustration = {
+    'john-molinero-john-molinero-the-sun-in-the-darkness.jpg': {
+      title: 'Sun in the Darkness',
+      description: 'No AI used for any portion.',
+    },
+    'john-molinero-anita.jpg': {
+      title: 'Anita',
+      description: 'No AI used for any portion.',
+    },
+    'john-molinero-burning-city.jpg': {
+      title: 'Burning City',
+      description: 'No AI used for any portion.',
+    },
+    'john-molinero-half-spirit.jpg': {
+      title: 'Half Spirit',
+      description: 'No AI used for any portion.',
+    },
+    'john-molinero-john-molinero-goblin-lookout.jpg': {
+      title: 'Goblin Lookout',
+      description: 'No AI used for any portion.',
+    },
+    'john-molinero-ranger.jpg': {
+      title: 'Ranger',
+      description: 'No AI used for any portion.',
+    },
+    'john-molinero-shambler-wight.jpg': {
+      title: 'Shambler Wight',
+      description: 'No AI used for any portion.',
+    },
+    'john-molinero-the-lost-tomb-of-the-wights.jpg': {
+      title: 'Lost Tomb of the Wights',
+      description: 'No AI used for any portion.',
+    },
+    'john-molinero-torchlight-full.jpg': {
+      title: 'Torchlight',
+      description: 'No AI used for any portion.',
+    },
+    'john-molinero-handmaiden.jpg': {
+      title: 'Handmaiden',
+      description: 'Oil on panel',
+    },
+    'john-molinero-old-man.jpg': {
+      title: 'Old Man',
+      description: 'Oil on panel',
+    },
+    'john-molinero-sea-maiden.jpg': {
+      title: 'Sea Maiden',
+      description: 'Oil on panel',
+    },
+    'john-molinero-sea-man.jpg': {
+      title: 'Sea Captain',
+      description: 'Oil on panel',
+    },
+    'john-molinero-angel.jpg': {
+      title: 'Angel',
+      description: 'No AI used for any portion.',
+    },
+  };
 
-    illustration = {
-        'john-molinero-john-molinero-the-sun-in-the-darkness.jpg': {
-            title: 'Sun in the Darkness',
-            description: 'No AI used for any portion.'
-        },
-        'john-molinero-anita.jpg': {
-            title: 'Anita',
-            description: 'No AI used for any portion.'
-        },
-        'john-molinero-burning-city.jpg': {
-            title: 'Burning City',
-            description: 'No AI used for any portion.'
-        },
-        'john-molinero-half-spirit.jpg': {
-            title: 'Half Spirit',
-            description: 'No AI used for any portion.'
-        },
-        'john-molinero-john-molinero-goblin-lookout.jpg': {
-            title: 'Goblin Lookout',
-            description: 'No AI used for any portion.'
-        },
-        'john-molinero-ranger.jpg': {
-            title: 'Ranger',
-            description: 'No AI used for any portion.'
-        },
-        'john-molinero-shambler-wight.jpg': {
-            title: 'Shambler Wight',
-            description: 'No AI used for any portion.'
-        },
-        'john-molinero-the-lost-tomb-of-the-wights.jpg': {
-            title: 'Lost Tomb of the Wights',
-            description: 'No AI used for any portion.'
-        },
-        'john-molinero-torchlight-full.jpg': {
-            title: 'Torchlight',
-            description: 'No AI used for any portion.'
-        },
-    };
+  constructor(host) {
+    this.render(host);
+  }
 
-    constructor(host) {
-        this.render(host);
-    }
+  render(host) {
+    const urlParams = new URLSearchParams(window.location.search);
+    const imageName = urlParams.get('image');
 
-    render(host) {
-        const urlParams = new URLSearchParams(window.location.search);
-        const imageName = urlParams.get('image');
+    const h2 = document.createElement('h2');
+    h2.innerText = this.illustration[imageName].title;
+    host.appendChild(h2);
 
-        const h2 = document.createElement('h2');
-        h2.innerText = this.illustration[imageName].title;
-        host.appendChild(h2);
+    const img = document.createElement('img');
+    img.setAttribute('src', `/img/art/${imageName}`);
+    img.setAttribute('alt', this.illustration[imageName].title);
+    host.appendChild(img);
 
-        const img = document.createElement('img');
-        img.setAttribute('src', `/img/art/${imageName}`);
-        img.setAttribute('alt', this.illustration[imageName].title);
-        host.appendChild(img);
-
-        const p = document.createElement('p');
-        p.innerText = this.illustration[imageName].description;
-        host.appendChild(p);
-    }
+    const p = document.createElement('p');
+    p.innerText = this.illustration[imageName].description;
+    host.appendChild(p);
+  }
 }
--- js/nav.js
+++ js/nav.js
@@ -1,30 +1,33 @@
 class Nav {
+  items = [
+    {
+      url: '/',
+      title: 'Illustration',
+    },
+    {
+      url: '/fine-art',
+      title: 'Fine Art',
+    },
+    {
+      url: '/about',
+      title: 'About',
+    },
+    {
+      url: 'https://www.artstation.com/johnmolinero',
+      title: 'ArtStation',
+    },
+    {
+      url: 'https://etsy.com/shop/JohnMolineroArt',
+      title: 'Store',
+    },
+  ];
 
-    items = [
-        {
-            'url': '/',
-            'title': 'Illustration',
-        },
-        {
-            'url': '/about',
-            'title': 'About',
-        },
-        {
-            'url': 'https://www.artstation.com/johnmolinero',
-            'title': 'ArtStation',
-        },
-        {
-            'url': 'https://etsy.com/shop/JohnMolineroArt',
-            'title': 'Store',
-        }
-    ];
-
-    constructor(host) {
-        this.items.forEach((item) => {
-            const el = document.createElement('a');
-            el.setAttribute('href', item.url);
-            el.innerText = item.title;
-            host.appendChild(el);
-        });
-    }
+  constructor(host) {
+    this.items.forEach((item) => {
+      const el = document.createElement('a');
+      el.setAttribute('href', item.url);
+      el.innerText = item.title;
+      host.appendChild(el);
+    });
+  }
 }
